290e3a3d9ae51aab35a2b4673066c17ecdd62074
Bash-and-tea <33610863+Bash-and-tea@users.noreply.github.com>
2018-01-18 08:37:29 -0500

    Update pip_config.bat

--- pip_config.bat
+++ pip_config.bat
@@ -2,5 +2,7 @@ cd C:\Python27
 python -m pip install --upgrade_pip
 cd C:\Python27\Scripts
 pip.exe install http
+pip.exe install wget
+pip.exe install cgi
 pip.exe install BaseHTTPServer
 echo Done
