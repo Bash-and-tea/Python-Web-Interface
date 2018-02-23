5969b00a0a21686e36208d767438d42c49cf9c9b
Isaac <33610863+Bash-and-tea@users.noreply.github.com>
2018-02-23 12:28:22 -0500

    Self-contained

--- pip_config.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Python27
-python -m pip install --upgrade_pip
-cd C:\Python27\Scripts
-pip.exe install http
-pip.exe install shutil
-pip.exe install cgi
-pip.exe install BaseHTTPServer
-echo Done
